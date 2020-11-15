dc75aad3d81a8de92b2a582e33a90061a908d7e7
1EternalDisciple1 <babraev98@mail.ru>
2020-11-15 14:36:41 +0500

    add netlify

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ const IndexPage = ({ data }) => {
             <Description />
             <SectionAdvantageIndex />
             <CommentSlider />
-            {/*увеличить margin в слайдере*/}
+
             <SectionReason />
 
             <SectionAppointment
