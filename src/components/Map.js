48bc6a0f5a426e354c802f87abaa0a2e6b68a387
1EternalDisciple1 <babraev98@mail.ru>
2020-11-20 09:31:53 +0500

    add english map

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,8 +4,8 @@ import React from "react"
 const Map = ()=> (
   <section className="section-map">
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d24179.28299703701!2d-73.698341!3d40.752998!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3fe2726e12738bdf!2sDr.%20Diana%20Gerov%20DDS!5e0!3m2!1sru!2sus!4v1597602045405!5m2!1sru!2sus"
-      width="100%" height="100%" frameBorder="0" style={{border: 0}}  allowFullScreen="" aria-hidden="false"
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48358.56599407402!2d-73.698341!3d40.752998!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3fe2726e12738bdf!2sDr.%20Diana%20Gerov%20DDS!5e0!3m2!1sen!2s!4v1605846063202!5m2!1sen!2s"
+      width="100%" height="100%" frameBorder="0" style={{border: 0}} allowFullScreen="" aria-hidden="false"
       tabIndex="0"></iframe>
   </section>
 )
