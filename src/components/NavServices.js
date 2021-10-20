421092b614460a45299de4c55ea99cb5b09439c9
uncleaar <75464863+GetmevaDev@users.noreply.github.com>
2021-10-20 12:45:19 +0600

    category fix

--- src/components/NavServices.js
+++ src/components/NavServices.js
@@ -6,7 +6,26 @@ const NavServices = () => {
 
   const data = useStaticQuery(graphql`
      query NavServicesQuery{
-        allStrapiServices {
+      NameCategory:  allStrapiCategorys {
+              edges {
+                node {
+                  NmaeCategory
+                 
+                }
+              }
+            }
+            NameCategory: allStrapiCategorys {
+                edges {
+                  node {
+                    NmaeCategory
+                    services {
+                      Title
+                      id
+                    }
+                  }
+                }
+              }
+        secondBlock: allStrapiServices(filter: {category: {NmaeCategory: {eq: "Core Dentistry"}}}) {
           edges {
             node {
               id
@@ -15,33 +34,104 @@ const NavServices = () => {
             }
           }
         }
+        thirdBlock: allStrapiServices(filter: {category: {NmaeCategory: {eq: "Preventative Dentistry"}}}) {
+          edges {
+            node {
+              id
+              Title
+              slug
+            }
+          }
+        }
+        fourthBlock: allStrapiServices(filter: {category: {NmaeCategory: {eq: "Cosmetic Dentistry"}}}) {
+    edges {
+      node {
+        id
+        Title
+        slug
+      }
+    }
+  }
      }
   `)
 
 
-
+console.log(data?.NameCategory.edges[1].node.services)
 
   return(
 
-    <div className="containers_sub-menu padding order_8 row">
+    <li className="containers_sub-menu padding order_8 row">
       <div className="container_sub-menu">
-      
+        <h3 className="heading_sub-menu">{data?.NameCategory?.edges[0]?.node?.NmaeCategory || null}</h3>
         <ul>
           {
-            data.allStrapiServices.edges.map(item => (
-              <li key={item?.node?.id || null}>
+           data?.NameCategory.edges[0].node.services.map(item => (
+              <li key={item?.id || null}>
+                {console.log(item)}
                 <a
                   href={`/services/${item?.node?.slug}`}
                   className="link_sub-menu">
-                  {item?.node?.Title || null}
+                  {item?.Title || null}
                 </a>
               </li>
             ))
           } 
-   
+      
         </ul>
       </div>
-    </div>
+      <div className="container_sub-menu">
+        <h3 className="heading_sub-menu">{data?.NameCategory?.edges[1]?.node?.NmaeCategory}</h3>
+        <ul>
+          {
+           data?.NameCategory.edges[1].node.services.map(item => (
+              <li key={item?.id || null}>
+                {console.log(item)}
+                <a
+                  href={`/services/${item?.node?.slug}`}
+                  className="link_sub-menu">
+                  {item?.Title || null}
+                </a>
+              </li>
+            ))
+          } 
+      
+        </ul>
+      </div>
+      <div className="container_sub-menu">
+        <h3 className="heading_sub-menu">{data?.NameCategory?.edges[2]?.node?.NmaeCategory}</h3>
+        <ul>
+        {
+           data?.NameCategory?.edges[2]?.node?.services?.map(item => (
+              <li key={item?.id || null}>
+                {console.log(item)}
+                <a
+                  href={`/services/${item?.node?.slug}`}
+                  className="link_sub-menu">
+                  {item?.Title || null}
+                </a>
+              </li>
+            ))
+          } 
+        </ul>
+      </div>
+      <div className="container_sub-menu">
+        <h3 className="heading_sub-menu">{data?.NameCategory?.edges[3]?.node?.NmaeCategory}</h3>
+        <ul>
+        {
+           data?.NameCategory?.edges[3]?.node?.services?.map(item => (
+              <li key={item?.id || null}>
+                {console.log(item)}
+                <a
+                  href={`/services/${item?.node?.slug}`}
+                  className="link_sub-menu">
+                  {item?.Title || null}
+                </a>
+              </li>
+            ))
+          } 
+        </ul>
+      </div>
+    </li>
     )
 }
 
@@ -49,3 +139,67 @@ const NavServices = () => {
 
 
 export default NavServices
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react"
+// import {graphql, useStaticQuery} from "gatsby"
+
+
+// const NavServices = () => {
+
+//   const data = useStaticQuery(graphql`
+//      query NavServicesQuery{
+//         allStrapiServices {
+//           edges {
+//             node {
+//               id
+//               Title
+//               slug
+//             }
+//           }
+//         }
+//      }
+//   `)
+
+
+
+
+//   return(
+
+//     <div className="containers_sub-menu padding order_8 row">
+//       <div className="container_sub-menu">
+      
+//         <ul>
+//           {
+//             data.allStrapiServices.edges.map(item => (
+//               <li key={item?.node?.id || null}>
+//                 <a
+//                   href={`/services/${item?.node?.slug}`}
+//                   className="link_sub-menu">
+//                   {item?.node?.Title || null}
+//                 </a>
+//               </li>
+//             ))
+//           } 
+   
+//         </ul>
+//       </div>
+//     </div>
+//     )
+// }
+
+
+
+
+// export default NavServices
