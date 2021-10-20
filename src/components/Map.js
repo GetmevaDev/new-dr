2ff2a3e7c9da853fd1fbaefc9012da7fa0865ef6
uncleaar <75464863+GetmevaDev@users.noreply.github.com>
2021-10-20 15:36:24 +0600

    map

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,12 +5,11 @@ const Map = ()=> (
   <section className="section-map">
     <iframe
       className={`lazload`}
-      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48358.56599407402!2d-73.698341!3d40.752998!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x3fe2726e12738bdf!2sDr.%20Diana%20Gerov%20DDS!5e0!3m2!1sen!2s!4v1605846063202!5m2!1sen!2s"
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12096.152029241515!2d-73.8746416!3d40.7171798!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc10891d527fcb068!2sIV%20STAT!5e0!3m2!1sru!2skz!4v1634722538002!5m2!1sru!2skz"
       width="100%" height="100%" frameBorder="0" style={{border: 0}} allowFullScreen="" aria-hidden="false"
       tabIndex="0"></iframe>
   </section>
 )
-
 
 
 export default Map
