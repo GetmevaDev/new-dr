e224d4aa483a3eed09d77d06c4f12ac1c5f4b617
uncleaar <75464863+GetmevaDev@users.noreply.github.com>
2021-10-26 14:20:49 +0600

    centered

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,15 +1,19 @@
 import React from "react"
 
-
-const Map = ()=> (
+const Map = () => (
   <section className="section-map">
     <iframe
       className={`lazload`}
-      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12096.152029241515!2d-73.8746416!3d40.7171798!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc10891d527fcb068!2sIV%20STAT!5e0!3m2!1sru!2skz!4v1634722538002!5m2!1sru!2skz"
-      width="100%" height="100%" frameBorder="0" style={{border: 0}} allowFullScreen="" aria-hidden="false"
-      tabIndex="0"></iframe>
+      src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d24192.303985803024!2d-73.874642!3d40.71718!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xc10891d527fcb068!2sIV%20STAT!5e0!3m2!1sen!2skz!4v1635236232660!5m2!1sen!2skz"
+      width="100%"
+      height="100%"
+      frameBorder="0"
+      style={{ border: 0 }}
+      allowFullScreen=""
+      aria-hidden="false"
+      tabIndex="0"
+    ></iframe>
   </section>
 )
-
 
 export default Map
