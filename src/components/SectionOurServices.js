f27b76ad366fbeac56cafe8367ead37dfdc80243
uncleaar <75464863+GetmevaDev@users.noreply.github.com>
2021-10-28 14:06:35 +0600

    color

--- src/components/SectionOurServices.js
+++ src/components/SectionOurServices.js
@@ -1,41 +1,40 @@
 import React from "react"
-import {graphql, useStaticQuery} from "gatsby"
-
+import { graphql, useStaticQuery } from "gatsby"
 
 const SectionOurServices = () => {
-
-const data = useStaticQuery(graphql`
-  query OurServicesQuery{
+  const data = useStaticQuery(graphql`
+    query OurServicesQuery {
       allStrapiServices(limit: 10) {
-    edges {
-      node {
-        id
-        Title
-        slug
+        edges {
+          node {
+            id
+            Title
+            slug
+          }
         }
       }
     }
-  }
-`)
-  return(
+  `)
+  return (
     <section className="section-services">
-      <h2 className="section-title">Our<strong>Services</strong></h2>
+      <h2 className="section-title">
+        Our<strong> Services</strong>
+      </h2>
 
       <div className="container row">
-
-        {
-          data.allStrapiServices.edges.map(elem =>{
-            const title = elem.node.Title || null;
-            const link = elem.node.slug
-            return(
-              <a
-                key={elem.node.id}
-                href={`/services/${link}`} className="link-services">{title}</a>
-            )
-          })
-        }
-
-
+        {data.allStrapiServices.edges.map(elem => {
+          const title = elem.node.Title || null
+          const link = elem.node.slug
+          return (
+            <a
+              key={elem.node.id}
+              href={`/services/${link}`}
+              className="link-services"
+            >
+              {title}
+            </a>
+          )
+        })}
       </div>
     </section>
   )
