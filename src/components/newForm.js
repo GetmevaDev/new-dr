4c044151bac9a2349170c64465700aa80add0440
uncleaar <75464863+GetmevaDev@users.noreply.github.com>
2021-11-05 16:24:05 +0600

    form

--- src/components/newForm.js
+++ src/components/newForm.js
@@ -1,137 +1,114 @@
 import React, { useState, useEffect } from "react"
+import emailjs from "emailjs-com"
 
 const newForm = () => {
-  const arr = [
-    { name: "Fatigue", id: 1 },
-    { name: "Fever", id: 2 },
-    { name: "Headache", id: 3 },
-    { name: "  Nausea abdominal pain", id: 4 },
-    { name: " Weight change.", id: 5 },
-    { name: "Vision problem", id: 6 },
-    { name: "Chest pain", id: 7 },
-    { name: "Irregular hear beat fainting", id: 8 },
-    { name: "Joint pain", id: 9 },
-    { name: "Headache", id: 10 },
-    { name: "Numbness", id: 11 },
-    { name: "Stress", id: 12 },
-    { name: "Depression", id: 13 },
-    { name: "Rectal bleeding", id: 14 },
-    { name: "Blood in urine", id: 15 },
-    { name: "Pregnant", id: 16 },
-  ]
+  const [checkbox, seCheckBox] = useState([
+    { name: "Fatigue", id: 1, value: "Fatigue" },
+    { name: "Fever", id: 2, value: "Fever" },
+    { name: "Headache", id: 3, value: "Headache" },
+    {
+      name: "abdominal_pain",
+      id: 4,
+      value: "Nausea abdominal pain",
+    },
+    { name: "Weight", id: 5, value: "Weight change." },
+    { name: "Vision", id: 6, value: "Vision problem" },
+    { name: "Chest ", id: 7, value: "Chest pain" },
+    {
+      name: "Irregular",
+      id: 8,
+      value: "Irregular hear beat fainting",
+    },
+    { name: "Joint ", id: 9, value: "Joint pain" },
+    { name: "Numbness", id: 11, value: "Numbness" },
+    { name: "Stress", id: 12, value: "Stress" },
+    { name: "Depression", id: 13, value: "Depression" },
+    { name: "Rectal ", id: 14, value: "Rectal bleeding" },
+    { name: "Blood ", id: 15, value: "Blood in urine" },
+    { name: "Pregnant", id: 16, value: "Pregnant" },
+  ])
 
-  // const [firstName, setFirstName] = useState()
-  // const [lastname, setLastname] = useState()
-  // const [wouldyoulike, setWouldYouLike] = useState()
-  // const [howDid, setHoWdid] = useState()
+  console.log(checkbox)
 
-  // const [email, setEmail] = useState("")
-  // const [emailDirty, setEmailDirty] = useState(false)
-  // const [emailError, setEmailError] = useState("Email is a required field")
-  // const [formValid, setFormValid] = useState(false)
-  // const [phone, setPhone] = useState()
-  // const history = useHistory()
-  // const emailHandler = e => {
-  //   setEmail(e.target.value)
-  //   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  const [firstName, setFirstName] = useState()
+  const [lastName, setLastname] = useState()
+  const [date, setDate] = useState()
+  const [address, setAddress] = useState()
+  const [phone, setPhone] = useState()
 
-  //   if (!re.test(String(e.target.value).toLowerCase())) {
-  //     setEmailError("Incorrect email")
-  //   } else {
-  //     setEmailError("")
-  //   }
-  // }
+  const [email, setEmail] = useState("")
+  const [emailDirty, setEmailDirty] = useState(false)
+  const [emailError, setEmailError] = useState("Email is a required field")
+  const [formValid, setFormValid] = useState(false)
+  const emailHandler = e => {
+    setEmail(e.target.value)
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-  // useEffect(() => {
-  //   if (emailError) {
-  //     setFormValid(false)
-  //   } else {
-  //     setFormValid(true)
-  //   }
-  // }, [emailError])
+    if (!re.test(String(e.target.value).toLowerCase())) {
+      setEmailError("Incorrect email")
+    } else {
+      setEmailError("")
+    }
+  }
 
-  // function sendEmail(e) {
-  //   e.preventDefault()
+  useEffect(() => {
+    if (emailError) {
+      setFormValid(false)
+    } else {
+      setFormValid(true)
+    }
+  }, [emailError])
 
-  //   emailjs
-  //     .sendForm(
-  //       "service_mw3qmm3",
-  //       "template_4329ukk",
-  //       e.target,
-  //       "user_iw2a3XOS7O7HrGbR8S31M"
-  //     )
-  //     .then(
-  //       result => {
-  //         console.log(result.text)
-  //         setFirstName("")
-  //         setLastname("")
-  //         setPhone("")
-  //         setEmail("")
-  //         setWouldYouLike("")
-  //         setHoWdid("")
-  //         history.push("/thankyou")
-  //       },
-  //       error => {
-  //         console.log(error.text)
-  //       }
-  //     )
-  // }
+  function sendEmail(e) {
+    e.preventDefault()
 
-  // const blurHandler = e => {
-  //   switch (e.target.name) {
-  //     case "email":
-  //       setEmailDirty(true)
-  //       break
-  //     default:
-  //       break
-  //   }
-  // }
+    emailjs
+      .sendForm(
+        "service_mwmcak2",
+        "template_18qrf1e",
+        e.target,
+        "user_UVUfWNWhv4kTBLfUHosjS"
+      )
+      .then(
+        result => {
+          console.log(result.text)
+
+          setFirstName("")
+          setLastname("")
+          setPhone("")
+          setAddress("")
+          setEmail("")
+          setDate("")
+          seCheckBox([])
+        },
+        error => {
+          console.log(error.text)
+        }
+      )
+  }
+
+  const blurHandler = e => {
+    switch (e.target.name) {
+      case "email":
+        setEmailDirty(true)
+        break
+      default:
+        break
+    }
+  }
 
   return (
     <div>
-      <form className="newForm container">
-        <div className="topNewForm">
-          <label>
-            <input
-              className="newformInput"
-              type="text"
-              placeholder="First Name"
-            />
-          </label>
-
-          <label>
-            <input
-              className="newformInput"
-              type="text"
-              placeholder="Last Name"
-            />
-          </label>
-
-          <label className="lastinput">
-            <input
-              className="newformInput"
-              type="date"
-              placeholder="Date of birth"
-            />
-          </label>
-        </div>
-
-        <div className="centerNewForm">
-          <div className="leftNewForm">
+      <div>
+        <form className="newForm container" onSubmit={sendEmail}>
+          <div className="topNewForm">
             <label>
               <input
                 className="newformInput"
                 type="text"
-                placeholder="Address"
-              />
-            </label>
-
-            <label>
-              <input
-                className="newformInput"
-                name="email"
-                type="email"
-                placeholder="E-mail"
+                name="firstName"
+                value={firstName}
+                placeholder="First Name"
               />
             </label>
 
@@ -139,106 +116,190 @@ const newForm = () => {
               <input
                 className="newformInput"
                 type="text"
-                placeholder="Current medications"
+                name="lastName"
+                value={lastName}
+                placeholder="Last Name"
+              />
+            </label>
+
+            <label className="lastinput">
+              <input
+                className="newformInput"
+                value={date}
+                type="date"
+                name="date"
+                placeholder="Date of birth"
               />
             </label>
           </div>
 
-          <div className="rightNewForm">
-            <label>
-              <input
-                className="newformInput"
-                name="phone"
-                value=""
-                placeholder="Phone"
-              />
-            </label>
+          <div className="centerNewForm">
+            <div className="leftNewForm">
+              <label>
+                <input
+                  className="newformInput"
+                  type="text"
+                  name="address"
+                  value={address}
+                  placeholder="Address"
+                />
+              </label>
 
-            <label>
-              <input
-                className="newformInput"
-                type="text"
-                placeholder="Allergies/drug reactions"
-              />
-            </label>
+              <label className="newformInput">
+                {emailDirty && emailError && (
+                  <div
+                    style={{
+                      color: "red",
+                      position: "relative",
+                      bottom: "-95px",
+                    }}
+                  >
+                    {emailError}
+                  </div>
+                )}
+                <input
+                  onChange={e => emailHandler(e)}
+                  value={email}
+                  placeholder="E-mail"
+                  onBlur={e => blurHandler(e)}
+                  className="newformInput"
+                  name="email"
+                  type="email"
+                />
+              </label>
 
-            <label>
-              <input
-                className="newformInput"
-                type="text"
-                placeholder="Current medical conditions"
-              />
-            </label>
+              <label>
+                <input
+                  className="newformInput"
+                  type="text"
+                  placeholder="Current medications"
+                />
+              </label>
+            </div>
+
+            <div className="rightNewForm">
+              <label>
+                <input
+                  className="newformInput"
+                  name="phone"
+                  value={phone}
+                  placeholder="Phone"
+                />
+              </label>
+
+              <label>
+                <input
+                  className="newformInput"
+                  type="text"
+                  placeholder="Allergies/drug reactions"
+                />
+              </label>
+
+              <label>
+                <input
+                  className="newformInput"
+                  type="text"
+                  placeholder="Current medical conditions"
+                />
+              </label>
+            </div>
           </div>
-        </div>
 
-        <div className="formTitle">
-          <div className="symptoms">
-            <h3>Symptoms</h3>
+          <div className="formTitle">
+            <div className="symptoms">
+              <h3>Symptoms</h3>
 
-            <div className="symptomsMain">
-              <div className="symptomsLeft">
-                {arr.slice(0, 8).map(({ name, id }) => (
-                  <label key={id}>
-                    <input type="checkbox" name="happy" value="yes" />
-                    <span>{name}</span>
-                  </label>
-                ))}
+              <div className="symptomsMain">
+                <div className="symptomsLeft">
+                  {checkbox.slice(0, 8).map(({ name, id, value }) => (
+                    <label key={id}>
+                      <input type="checkbox" name={name} value={value} />
+                      <span>{value}</span>
+                    </label>
+                  ))}
+                </div>
+
+                <div className="symptomsRight">
+                  {checkbox.slice(8, 16).map(({ name, id, value }) => (
+                    <label key={id}>
+                      <input type="checkbox" name={name} value={value} />
+                      <span>{value}</span>
+                    </label>
+                  ))}
+                </div>
               </div>
+            </div>
 
-              <div className="symptomsRight">
-                {arr.slice(8, 16).map(({ name, id }) => (
-                  <label key={id}>
-                    <input type="checkbox" name="happy" value="yes" />
-                    <span>{name}</span>
-                  </label>
-                ))}
+            <div className="serviceInterestedForm">
+              <h3>Service interested in</h3>
+              <div className="serviceInterestedForm_inner">
+                <label>
+                  <input
+                    type="checkbox"
+                    name="IV_Infusion"
+                    value="IV Infusion"
+                  />
+                  <span>IV Infusion </span>
+                </label>
+
+                <label>
+                  <input
+                    type="checkbox"
+                    name="Microneedling"
+                    value="Microneedling"
+                  />
+                  <span>Microneedling </span>
+                </label>
+
+                <label>
+                  <input type="checkbox" name="PRP" value="PRP" />
+                  <span>PRP</span>
+                </label>
+
+                <label>
+                  <input type="checkbox" name="BOTOX" value="Botox" />
+                  <span>Botox</span>
+                </label>
+              </div>
+            </div>
+            <div className="packagesForm">
+              <h3>Packages</h3>
+
+              <div className="packagesForm_inner">
+                <label>
+                  <input
+                    type="checkbox"
+                    name="I Vitamin"
+                    value="I Vitamin B 12 intramuscular injection $30"
+                  />
+                  <span>I Vitamin B 12 intramuscular injection $30 </span>
+                </label>
+
+                <label>
+                  <input
+                    type="checkbox"
+                    name="Bit D3"
+                    value="Bit D3 intramuscular or subcutaneous injection $30"
+                  />
+                  <span>
+                    Bit D3 intramuscular or subcutaneous injection $30{" "}
+                  </span>
+                </label>
               </div>
             </div>
           </div>
-
-          <div className="serviceInterestedForm">
-            <h3>Service interested in</h3>
-            <div className="serviceInterestedForm_inner">
-              <label>
-                <input type="checkbox" name="happy" value="yes" />
-                <span>IV Infusion </span>
-              </label>
-
-              <label>
-                <input type="checkbox" name="happy" value="yes" />
-                <span>Microneedling </span>
-              </label>
-
-              <label>
-                <input type="checkbox" name="happy" value="yes" />
-                <span>PRP</span>
-              </label>
-
-              <label>
-                <input type="checkbox" name="happy" value="yes" />
-                <span>Botox</span>
-              </label>
-            </div>
+          <input
+            disabled={!formValid}
+            className="newFormSubmit"
+            type="submit"
+            value="Submit"
+          />
+          <div className="newFormIconBlock">
+            <span className="newFormIcon"></span>
+            <p>Email to us ensuring it is all secure and encrypted info.</p>
           </div>
-          <div className="packagesForm">
-            <h3>Packages</h3>
-
-            <div className="packagesForm_inner">
-              <label>
-                <input type="checkbox" name="happy" value="yes" />
-                <span>I Vitamin B 12 intramuscular injection $30 </span>
-              </label>
-
-              <label>
-                <input type="checkbox" name="happy" value="yes" />
-                <span>Bit D3 intramuscular or subcutaneous injection $30 </span>
-              </label>
-            </div>
-          </div>
-        </div>
-        <input className="newFormSubmit" type="submit" value="Submit" />
-      </form>
+        </form>
+      </div>
     </div>
   )
 }
